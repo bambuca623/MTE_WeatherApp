ae25033dc98893f32ca77dba8a31451addd44c24
bambuca623 <44742342+bambuca623@users.noreply.github.com>
2019-02-02 00:04:07 +0100

    Implementace tříd
    Implemetace tříd MapWeather a DataParser

--- app/src/main/java/com/example/adamh/mte_weatherapp/MapWeather.java
+++ app/src/main/java/com/example/adamh/mte_weatherapp/MapWeather.java
@@ -1,4 +1,48 @@
 package com.example.adamh.mte_weatherapp;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
 public class MapWeather {
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String APPID = "6e1239278a276a67c8f27841b004b90a";
+
+    public String getWeatherData(String location) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
+        try {
+            con = (HttpURLConnection) ( new URL(BASE_URL + location + "&APPID=" + APPID)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while ( (line = br.readLine()) != null )
+                buffer.append(line + "rn");
+
+            is.close();
+            con.disconnect();
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+    }
+
 }
